d6ed4c4eafec832ddf6f86d82a2c76e91592c266
civarry <cjcarito15@gmail.com>
2024-11-02 20:50:40 +0800

    Updated configuration

--- app.py
+++ app.py
@@ -116,5 +116,4 @@ def utility_processor():
     return dict(current_step=session.get('current_step', 0))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Get the port from the environment
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
